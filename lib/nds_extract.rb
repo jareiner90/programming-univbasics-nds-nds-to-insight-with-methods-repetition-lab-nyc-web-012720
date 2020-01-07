def78da24fd72b6d0284686c78fe8269ee17d532
John Reiner <johnreiner1990@gmail.com>
2020-01-07 19:52:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -58,4 +58,5 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  return grand_total
 end
